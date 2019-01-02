25e3eb42f5f49f2d802c16d89e766b1607560774
AlBeRNs5 <46230913+AlBeRNs5@users.noreply.github.com>
2019-01-02 23:52:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -239,6 +239,24 @@ client.on('message', message => {
        });
 
 
+client.on('message', message => {
+    if (message.content === ('*bot')) {
+    message.channel.send({
+        embed: new Discord.RichEmbed()
+            .setAuthor(client.user.username,client.user.avatarURL)
+            .setThumbnail(client.user.avatarURL)
+            .setColor('RANDOM')
+            .addField('**Bot Ping**🚀 :' , [`${Date.now() - message.createdTimestamp}` + 'MS'], true)
+            .addField('**Servers**📚 :', [client.guilds.size], true)
+            .addField('**Channels**📝 :' , `[ ${client.channels.size} ]` , true)
+            .addField('**Users**🔮 :' ,`[ ${client.users.size} ]` , true)
+            .addField('**Bot Name**🔰 :' , `[ ${client.user.tag} ]` , true)
+            .addField('**Bot Owner**👑 :' , `[<@441617122343256070>]` , true)
+            .setFooter(message.author.username, message.author.avatarURL)
+    })
+}
+});
+
 
 
 client.login(process.env.BOT_TOKEN);
