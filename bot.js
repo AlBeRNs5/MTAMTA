91be5c603d84c92236e078bfb25a91b3ba97259e
AlBeRNs5 <46230913+AlBeRNs5@users.noreply.github.com>
2019-01-02 04:05:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,6 +32,7 @@ client.user.setGame(`AlBeRNs  `,"https://www.twitch.tv/dggamingbot")
  
 
 
+
 client.on('message', message => {
     if (message.content.startsWith("<@529791622448152596>"))
     
@@ -42,6 +43,33 @@ client.on('message', message => {
 
 
 });
+
+
+
+client.on('message', msg => {
+  if (msg.author.bot) return;
+  if (!msg.content.startsWith(prefix)) return;
+  let command = msg.content.split(" ")[0];
+  command = command.slice(prefix.length);
+  let args = msg.content.split(" ").slice(1);
+ 
+    if(command === "clear") {
+        const emoji = client.emojis.find("name", "wastebasket")
+    let textxt = args.slice(0).join("");
+    if(msg.member.hasPermission("MANAGE_MESSAGES")) {
+    if (textxt == "") {
+        msg.delete().then
+    msg.channel.send("***```Supply A Number ًں‘Œ```***").then(m => m.delete(3000));
+} else {
+    msg.delete().then
+    msg.delete().then
+    msg.channel.bulkDelete(textxt);
+        msg.channel.send("```Cleard: " + textxt + "\n Messages```").then(m => m.delete(3000));
+        }    
+    }
+}
+});
+
 
 
 client.login(process.env.BOT_TOKEN);
