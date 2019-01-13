9a3c42e939fdea6318d86aa464ec84cd97b1dafd
AlBeRNs5 <46230913+AlBeRNs5@users.noreply.github.com>
2019-01-13 17:27:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -280,6 +280,78 @@ client.on('guildMemberAdd', member => {
 });
 
 
+client.on('message', message => {
+    if (message.author.bot) return;
+     if (message.content === prefix + "help") {
+      if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
+     message.channel.send('**تم ارسال رسالة في الخاص**');
+
+
+
+
+ message.author.sendMessage(`
+ **
+[❖═════ General Commands ═══════❖]
+
+ #id معلومات عن حسابك الشخصي
+
+ #server معلومات حول السيرفر
+ 
+ #move سحب عضو الى رومك الصوتي
+
+ #clear مسح الرسائل الموجوده في الروم بعدد
+
+ #avatar يعرض اك صورتك الشخصية
+ 
+ #image يعرض لك صورة السيرفر
+ 
+ #credit يوريك كم الكريديت حقتك
+
+ #daily يسوي لك سحب فلوس
+
+ #rep يعطي ريب
+
+ #profile معلومات عامة مع الصورة
+ 
+[❖═════ Administrator Commands ═══════❖]
+
+ #ban حضر عضو من السيرفر
+ 
+ #kick طرد عضو من السيرفر
+ 
+ #mute اعضاء ميوت كتابي لعضو في السيرفر
+ 
+ #unmute فك الميوت عن عضو في السيرفر
+ 
+ #dac حذف جميع رومات السيرفر
+ 
+ #dar حذف جميع رتب السيرفر
+ 
+ #openroom فتح المحادثة في الروم
+ 
+ #closeroom قفل المحادثة في الرةوم
+
+ #role اعطاء رتبه لشخض معين
+ 
+ #role humans اعطاء رتب للبشريين
+ 
+ #role bots اعطاء رتبه للبوتات
+ 
+ #role all اعطاء رتبه للجميع سواء بشر او بوتات
+ 
+[❖═════ Other ═══════❖]
+
+ #support رابط سيرفر الدعم الفني
+ 
+ #invite رابط اضافة البوت
+
+ **`);
+
+    }
+});
+
+
+
 
 
  
