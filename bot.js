070ed8cda101c70169c6d0566706f1bb22813b9e
AlBeRNs5 <46230913+AlBeRNs5@users.noreply.github.com>
2019-01-13 23:56:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -513,6 +513,30 @@ message.channel.sendEmbed(embed)
 }
 });
 
+client.on('message', message => {
+    var prefix = "#"
+    if (message.content === prefix + 'createText') {
+        if(!message.channel.guild) return message.channel.send('**This Command Only For Servers !**')
+                if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send(`**${message.author.username} You Dont Have** ``MANAGE_CHANNELS`` **Premission**`);
+         message.guild.createChannel('اسم الروم الكتابي', 'text')
+         message.guild.createChannel('اسم الروم الصوتي', 'voice')
+          message.guild.createChannel('اسم الروم الكتابي', 'text')
+         message.guild.createChannel('اسم الروم الصوتي', 'voice')
+          message.guild.createChannel('اسم الروم الكتابي', 'text')
+         message.guild.createChannel('اسم الروم الصوتي', 'voice')
+         message.guild.createChannel('اسم الروم الكتابي', 'text')
+         message.guild.createChannel('اسم الروم الصوتي', 'voice')
+          message.guild.createChannel('اسم الروم الكتابي', 'text')
+         message.guild.createChannel('اسم الروم الصوتي', 'voice')
+         message.guild.createChannel('اسم الروم الكتابي', 'text')
+         message.guild.createChannel('اسم الروم الصوتي', 'voice')
+                  message.guild.createChannel('اسم الروم الكتابي', 'text')
+         message.guild.createChannel('اسم الروم الصوتي', 'voice')
+    
+    message.channel.sendMessage('**Text Channel Was Succsesfluy Created**')
+    }
+    });
+
 
 
 client.login(process.env.BOT_TOKEN);
