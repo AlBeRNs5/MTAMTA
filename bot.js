a0b5966b15febd0eca60229e870af894e38bef8e
AlBeRNs5 <46230913+AlBeRNs5@users.noreply.github.com>
2019-01-15 12:05:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -519,7 +519,7 @@ client.on('message', message => {
         if(!message.channel.guild) return message.channel.send('**This Command Only For Servers !**')
                 if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send(`**${message.author.username} You Dont Have** ``MANAGE_CHANNELS`` **Premission**`);
          message.guild.createChannel('announcements', 'text')
-         message.guild.createChannel('Scripting-show', 'voice')
+         message.guild.createChannel('Scripting-show', 'text')
           message.guild.createChannel('maps-show', 'text')
          message.guild.createChannel('talk', 'voice')
           message.guild.createChannel('vote', 'text')
