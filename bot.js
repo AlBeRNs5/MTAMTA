be1c8fa8de0595f646ba3edfa1f65b2b389739fc
AlBeRNs5 <46230913+AlBeRNs5@users.noreply.github.com>
2019-01-16 16:57:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -539,6 +539,24 @@ client.on('message', message => {
     }
     });
 
+client.on("message", msg => {
+  var prefix = '/'//البركفس
+  if(msg.content.startsWith(prefix + 'myguild')){
+    let embed = new Discord.RichEmbed()
+    .setThumbnail(msg.guild.iconURL)
+    .setColor("RANDOM")
+    .addField("Year📆",msg.guild.createdAt.getFullYear())
+    .addField("Hour📆", msg.guild.createdAt.getHours())
+    .addField("Day📆", msg.guild.createdAt.getDay())
+    .addField("Month📆", msg.guild.createdAt.getMonth())
+    .addField("Minutes📆", msg.guild.createdAt.getMinutes())
+    .addField("Seconds📆", msg.guild.createdAt.getSeconds())
+    .addField("Full📆", msg.guild.createdAt.toLocaleString())
+    .setTimestamp()
+    msg.channel.send(embed);
+  }
+});
+
 
 
 
