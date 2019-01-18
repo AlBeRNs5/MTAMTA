3364bc6dcf05eb3dc7999538817225c020fbe503
AlBeRNs5 <46230913+AlBeRNs5@users.noreply.github.com>
2019-01-18 21:05:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1107,6 +1107,23 @@ message.channel.send(`**Top 10 Servers : **\n1. **${top[0].name}**: ${top[0].mem
 }
   });
 
+client.on("message", msg => {
+  if(msg.content === '/' + "id") {
+      const embed = new Discord.RichEmbed();
+  embed.addField("🔱| اسم الحساب :", `${msg.author.username}#${msg.author.discriminator}`, true)
+          .addField("🆔| الاي دي :", `${msg.author.id}`, true)
+          .setColor("RANDOM")
+          .setFooter(msg.author.username , msg.author.avatarURL)
+          .setThumbnail(`${msg.author.avatarURL}`)
+          .setTimestamp()
+          .setURL(`${msg.author.avatarURL}`)
+          .addField('📛| الحالة :', `${msg.author.presence.status.toUpperCase()}`, true)
+          .addField('🎲| بلاينج :', `${msg.author.presence.game === null ? "No Game" : msg.author.presence.game.name}`, true)
+   .addField(':military_medal:  الرتب', `**[ ${msg.member.roles.filter(r => r.name).size} ]**`, true)
+          .addField('📅| تم الانضمام للديسكورد في :', `${msg.createdAt}`,true);
+      msg.channel.send({embed: embed})
+  }
+})
 
 
 
