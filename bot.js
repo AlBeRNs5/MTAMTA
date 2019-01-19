56c9b6fdeb0a5cefc285ec7d73efbfd8dd65512f
AlBeRNs5 <46230913+AlBeRNs5@users.noreply.github.com>
2019-01-19 16:32:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1179,21 +1179,6 @@ client.on('message', message => {
     }); 
 
 
-var prefix = "/";
-
-client.on("message", message => {//
-
-            if (message.content.startsWith(prefix + "obc")) {//
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); //
-  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {//
- m.send(`${argresult}\n ${m}`);//
-})//
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
- message.delete(); 
-};     //
-}); //
 
 
 client.on('guildMemberAdd', member => {
