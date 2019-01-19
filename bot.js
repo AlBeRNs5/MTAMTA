37a3eafc245499751bc429732e71a2f5eb207743
AlBeRNs5 <46230913+AlBeRNs5@users.noreply.github.com>
2019-01-19 15:30:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1088,6 +1088,69 @@ client.on("message", msg => {
   }
 })
 
+client.on('message', message => {
+    if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('/bcall')){
+if(!message.author.id === '441617122343256070') return;
+message.channel.sendMessage('جار ارسال الرسالة |✅')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
+
+client.on('message', message => {
+    if(message.content == prefix + 'slist') {
+             if(!message.author.id === '323885452207587329') return;
+             if(!message.author.id === '334435543851204618') return;
+    var gimg;
+    var gname;
+    var gmemb;
+    var gbots;
+    var groles;
+    var servers = client.guilds;
+    servers.forEach((g)=>{// 
+    gname = g.name; // 
+    gimg = g.iconURL;// 
+    gmemb = g.members.size;// 
+    let serv = new Discord.RichEmbed()//
+    .setAuthor(gname,gimg)//
+    .setThumbnail(gimg)//
+    .addField('Server Member Count',gmemb = g.members.size)
+    .setColor('RANDOM')
+    message.channel.send(`
+   
+            `);
+          message.channel.sendEmbed(serv);
+    }) 
+    }
+    });
+   
+    client.on('message', message => {
+    if(message.content == prefix + 'slis') {
+             if(!message.author.id === '323885452207587329') return;  
+             if(!message.author.id === '334435543851204618') return;
+    var gimg;
+    var gname;
+    var gmemb;
+    var gbots;
+    var groles;
+    var servers = client.guilds;
+    servers.forEach((g)=>{
+    gname = g.name;
+    gimg = g.iconURL;
+    gmemb = g.members.size;
+    let serv = new Discord.RichEmbed()
+    message.channel.send(`
+    **-------------------------**
+      Server Name : **${gname}**
+      Server MemberCount : **${gmemb} **
+      **---------------------------**
+            `);
+    })
+    }
+    });
 
 
 
