a077e4c2f66418fa58fe58219834162d25ef208d
AlBeRNs5 <46230913+AlBeRNs5@users.noreply.github.com>
2019-01-22 03:01:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -807,37 +807,33 @@ hours = 12;
  }
 });
 
-client.on('message', message => {
-    var p = message.mentions.members.first();
-    var reason = message.content.split(" ").slice(2).join(' ');
-    var log = message.guild.channels.find('name', 'log');
-    if(message.content.startsWith(`${prefix}ban`)){
-        if(!p) return message.reply(`**Mention the user!**`);
-        if(!reason) return message.reply(`**Spofic a reason!**`);
-        if(!p.bannable) return message.reply(`**I can't ban a staff member!**`);
-        reason = reason.replace('0', "**نشر في الخاص**");
-        reason = reason.replace('1', "**اسم غير لائق**");
-        reason = reason.replace('2', "**صوره غير لائقه**");
-        reason = reason.replace('3', "**سب الاهل**");
-        reason = reason.replace('4', "**سب الذات الاهيه**");
-        reason = reason.replace('5', "**مخالفه القوانين مع اخذ اكثر من تحذير**");
-        reason = reason.replace('6', "**سبام في الشات**");
-        reason = reason.replace('7', "**استخدام بعض الاوامر بشكل مسبب للإضرار بالسيرفر**");
-        reason = reason.replace('8', "**جلب اعضاء مفبركين للسيرفر**");
-        reason = reason.replace('9', "**عنصريه**");
-        var embed = new Discord.RichEmbed()
-        .setAuthor(`User Banned!`)
-        .addField(`Name ♣`, `<@${p.id}>`)
-        .addField(`By ♣`, `<@${message.author.id}>`)
-        .addField(`Reason ♣`, reason)
-        .setTimestamp()
-        .setColor("BLACK")
-        .setFooter(` `)
-        p.ban();
-            message.delete();
-        log.send({embed});
-    }
-});
+client.on('message' , message => {
+  if (message.author.dark) return;
+  if (!message.content.startsWith(prefix)) return;
+ 
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+ 
+  let args = message.content.split(" ").slice(1);
+ 
+  if (command == "ban") {
+               if(!message.channel.guild) return message.reply('** This command only for servers**');
+         
+  if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**You Don't Have ` BAN_MEMBERS ` Permission**");
+  if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
+  let user = message.mentions.users.first();
+  let reason = message.content.split(" ").slice(2).join(" ");
+      /*let banlog = client.channels.find("name", "ban-log");
+  if(!banlog) return message.reply("I've detected that this server doesn't have a ban-log text channel.");*/
+  if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
+  if(!reason) return message.reply ("**اكتب سبب الباند**");
+  if (!message.guild.member(user)
+  .bannable) return message.reply("**لايمكنني ابند شخص اعلى من رتبتي يرجه اعطاء البوت رتبه عالي**");
+ 
+  message.guild.member(user).ban(7, user);
+  message.channel.sendMessage("**لقد تم اعطاء الباند الي شخص بنجاح** ✅");
+}
+}); 
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
